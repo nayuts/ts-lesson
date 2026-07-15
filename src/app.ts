8b8470888376a3b1aff85c3e75840e272c8de96a
nayuta <na72.lamb@gmail.com>
2026-07-16 00:16:15 +0900

    課題3まで

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,24 @@
-const message: string = "Hello World, TypeScript!";
-console.log(message);
+// const message: string = "Hello World, TypeScript!";
+// console.log(message);
+
+// const message: string = "エラーテスト";
+
+// interface users {
+//   name: string;
+//   contact: string;
+//   email: string;
+// }
+
+// 2秒後にTodo一覧を返す関数
+function fetchTodos(): Promise<string[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(["買い物", "掃除"]), 2000);
+  });
+}
+
+// 2秒後にユーザー情報を返す関数
+function fetchUser(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("山田太郎"), 2000);
+  });
+}
